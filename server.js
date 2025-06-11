fc6f50bcf0dde01e4d9294c8008771a299c11fd2
Prince Owire <princeowire131@gmail.com>
2025-06-12 00:27:29 +0100

    Add initial implementation of message sender application with HTML and server setup

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    const filePath = path.join(__dirname, 'public', 'index.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading HTML');
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  } else if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => (body += chunk));
+    req.on('end', () => {
+      const message = new URLSearchParams(body).get('message');
+      console.log('Message received:', message);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(message); // return message to frontend
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
